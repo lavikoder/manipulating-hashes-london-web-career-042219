74425fb4e477bb9c8076621a10c87792ed42b563
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-30 14:47:51 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,8 +19,8 @@ def first_challenge
         #at this level, person is freddy and data it the hash of key/value pairs
         #iterating over the "data" hash, will output the second level of data we need
     
-      data.each do |attribute, value|
-        if attribute == :favorite_icecream_flavors 
+      data[:favorite_icecream_flavors].each do |attribute, value|
+        if value == "strawberry" 
           value.each {|flavour| attributes.delete("strawberry")}
         end 
              #remember to return your newly altered contacts hash!
